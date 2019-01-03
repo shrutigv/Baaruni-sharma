bca9bc454f32701465c00082e08ac850d0dbd178
GV <Shruti.GV@philips.com>
2019-01-03 16:40:59 +0530

    fixing errors

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app" class="background-change">
-	  <div class="navigation">
-     	<ul>
+	<div id="app" class="background-change">
+		<div class="navigation">
+			<ul>
 				<li><router-link to="/">Home</router-link></li>
 				<li><router-link to="/lastestUpdate">Latest update</router-link></li>
 				<li><router-link to="/instagram">Instagram</router-link></li>
@@ -9,8 +9,8 @@
 				<li><router-link to="/about">About</router-link></li>
 				<li><router-link to="/invitation">Invite</router-link></li>
 			</ul>
-    </div>
-	  <router-view></router-view>
+		</div>
+		<router-view></router-view>
   </div>
 </template>
 
@@ -19,9 +19,6 @@ import Home from './components/Home.vue'
 import Youtube from './components/Youtube.vue'
 import Instagram from './components/Instagram.vue'
 import About from './components/About.vue'
-import VueResource from 'vue-resource';
-
-Vue.use(VueResource);
 
 export default {
   name: 'App',
