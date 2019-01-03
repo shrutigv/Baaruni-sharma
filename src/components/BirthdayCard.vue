c1dd52b7130fdb947b8504b7cda3fb71418a1689
GV <Shruti.GV@philips.com>
2019-01-03 18:20:51 +0530

    css

--- src/components/BirthdayCard.vue
+++ src/components/BirthdayCard.vue
@@ -42,7 +42,10 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+.navigation{
+  display:none;
+}
 h1{
       font-size: 35px;
 }
